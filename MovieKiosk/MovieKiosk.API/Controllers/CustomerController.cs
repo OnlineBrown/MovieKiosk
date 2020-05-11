c2b73034c7a2b07ef5d1caa787a5960903dfa472
Sean Brown <sean@onlinebrown.com>
2020-05-10 23:08:31 -0700

    combined customer api

---
+++ MovieKiosk/MovieKiosk.API/Controllers/CustomerController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MovieKiosk.API.Models;
+
+
+namespace MovieKiosk.API.Controllers
+{ 
+    public class CustomerController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public string Post([FromBody]Models.Customer customer)
+        {
+            return Data.Customer.AddCustomer(customer);
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
